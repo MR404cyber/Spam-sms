a11d4bf734e6540596a136ad3e06278ed7bc265f
MR404cyber <75874865+MR404cyber@users.noreply.github.com>
2020-12-16 16:09:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-(figlet aku mencintaimu rahma)
+def main():
+    time.sleep(1)
+    os.system ('clear')
+    print '[96m'
+    os.system ('figlet i love rahma')
+    print'[92m================================='
+    print' Whatshapp   : 082331072836 '
+    print'[97m Author      : [96mMr.404 '
+    print'[97m Team        : [96mAnonymous '
